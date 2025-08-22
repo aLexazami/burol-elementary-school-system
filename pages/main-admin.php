894247a0244d173cc4c7d0b6c2d23df22574acb7
Alex Flores <alexazamiflores123@gmail.com>
2025-08-22 15:29:45 +0800

    Makes some chnages in Admin Dashboard

--- pages/main-admin.php
+++ pages/main-admin.php
@@ -30,7 +30,7 @@ $annualCount = getRespondentCount('annual', $conn);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="../src/styles.css" rel="stylesheet">
-  <title>Burol Elementary School</title>
+  <title>Admin Dashboard</title>
 </head>
 
 <body class="bg-gradient-to-b from-white to-emerald-800 min-h-screen flex flex-col">
@@ -38,36 +38,36 @@ $annualCount = getRespondentCount('annual', $conn);
   <!-- Header Section -->
     <?php include '../includes/header.php' ?>
 
-  <!-- Main Content Section -->
-  <main class=" grid grid-cols-[248px_1fr] gap-4 mt-2 m-2">
+  <!-- Admin Main Content Section -->
+  <main class=" grid grid-cols-[248px_1fr] gap-4 m-2 h-screen">
   <!-- Left Side Navigation -->
     <?php include '../includes/side-nav-admin.php' ?>
 
 
   <!-- Right Side Content -->
   <section class="bg-white p-2">
-    <div class="bg-green-700 p-2 grid grid-cols-3 gap-4 mt-6">
-      <div class="bg-blue-200 col-span-3">
+    <div class="p-2 grid grid-cols-3 gap-4 mt-6">
+      <div class="bg-gray-300 col-span-3  p-2">
         <h1 class="font-bold text-center text-lg">Respondent</h1>
       </div>
 
       <!-- New -->
       <div class="bg-white shadow-md rounded-lg p-4 text-center">
-        <img src="/assets/icons/new.png" class="mx-auto h-6 w-6 mb-2">
-        <p class="text-sm text-gray-500 uppercase">New</p>
+        <img src="/assets/img/new.png" class="mx-auto h-15 w-15 mb-2  ">
+        <p class="text-sm text-gray-500 uppercase ">New</p>
         <p class="text-2xl font-bold "><?= $newCount ?></p>
       </div>
 
       <!-- Weekly -->
       <div class="bg-white shadow-md rounded-lg p-4 text-center">
-        <img src="/assets/icons/weekly.png" class="mx-auto h-6 w-6 mb-2">
+        <img src="/assets/img/weekly.png" class="mx-auto h-15 w-15 mb-2 ">
         <p class="text-sm text-gray-500 uppercase">Weekly</p>
         <p class="text-2xl font-bold"><?= $weeklyCount ?></p>
       </div>
 
       <!-- Total -->
       <div class="bg-white shadow-md rounded-lg p-4 text-center">
-        <img src="/assets/icons/total.png" class="mx-auto h-6 w-6 mb-2">
+        <img src="/assets/img/total.png" class="mx-auto h-15 w-15 mb-2 ">
         <p class="text-sm text-gray-500 uppercase">Total</p>
         <p class="text-2xl font-bold "><?= $annualCount ?></p>
       </div>
