6de4a47a0f805420eded7a9ca902398aa3702cde
Alex Flores <alexazamiflores123@gmail.com>
2025-08-24 02:46:26 +0800

    Add a file for auth-check

--- controllers/auth-check.php
+++ controllers/auth-check.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
-require_once __DIR__ . '/config.php';
-
 if (!isset($_SESSION['username'])) {
-header("Location: " . BASE_URL . "index.php");
-exit();
+    header("Location: /index.php");
+    exit();
 }
 ?>
+
